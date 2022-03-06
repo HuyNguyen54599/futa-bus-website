a34252fd20e129fe2fec8fb0c2f0b149f46dfd00
Nguyen Ngoc Huy <huynguyenffn@gmail.com>
2022-03-06 22:54:19 +0700

    header and hero

---
+++ public/scripts/header.js
@@ -0,0 +1,24 @@
+const menuToggle = document.querySelector('.header-toggle');
+const header = document.querySelector('.header');
+const sideMenu = document.querySelector('.header-side-menu');
+
+menuToggle.onclick = (e) => {
+    header.classList.toggle('header-toggle-active');
+}
+
+window.onresize = (e) => {
+    if (screen.width > 969) {
+        header.classList.remove('header-toggle-active');
+    }
+}
+window.onclick = (e) => {
+    header.contains(e.target) || e.target.matches('.header-toggle') || header.classList.remove('header-toggle-active');
+}
+
+window.onscroll = (e) => {
+    if (document.body.scrollTop > 102 || document.documentElement.scrollTop > 102) {
+        header.classList.add('header-up');
+    } else {
+        header.classList.remove('header-up');
+    }
+}
