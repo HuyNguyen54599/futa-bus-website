a863b24927c21e5a6679ce91b94affa8c36c97f5
Nguyen Ngoc Huy <huynguyenffn@gmail.com>
2022-03-10 21:36:00 +0700

    recommend

--- public/vendors/swiperSlider.js
+++ public/vendors/swiperSlider.js
@@ -30,3 +30,54 @@ const swiper = new Swiper('.swiper', {
     //     }
     // }
 });
+
+const secondSwiper = new Swiper('.recommend-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    speed: 800,
+    autoplay: {
+        delay: 2000,
+    },
+
+    breakpoints: {
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 7,
+            spaceBetween: 70
+        },
+        // when window width is >= 1024px
+        1024: {
+            slidesPerView: 6,
+            spaceBetween: 50
+        },
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 5,
+            spaceBetween: 70
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 4,
+            spaceBetween: 50
+        },
+        400: {
+            slidesPerView: 3,
+            spaceBetween: 50
+        },
+        300: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+
+    },
+    // If we need pagination
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.recommend-swiper-button-next',
+        prevEl: '.recommend-swiper-button-prev',
+    },
+});
