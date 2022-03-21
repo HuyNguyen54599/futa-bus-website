e3adb8cf11def8db5a4c7906838e28581024cb86
Nguyen Ngoc Huy <huynguyenffn@gmail.com>
2022-03-21 16:26:49 +0700

    complete 2.1

--- public/scripts/bookingStep2.js
+++ public/scripts/bookingStep2.js
@@ -32,3 +32,36 @@ function onlyOneCheckedActive(allCheckBox, checkbox) {
         routeScheduleLists[index].classList.remove('route-schedule-active');
     })
 }
+
+//active floor on responsive screen
+let routeSeats = [...document.querySelectorAll('.route-seat')];
+
+routeSeats.forEach((item, index) => {
+    let buttonFloorControls = [...item.querySelectorAll('.route-seat-title div')];
+    let floors = [...item.querySelectorAll('.route-seat-floor-item')];
+
+    buttonFloorControls.forEach((ele, ind) => {
+        ele.onclick = () => {
+            if (screen.width <= 767) {
+                removeFloorControlActive(buttonFloorControls);
+                addFloorNone(floors);
+
+                ele.classList.add('seat-title-active');
+
+                floors[ind].classList.remove('this-floor-not');
+            }
+        }
+    })
+})
+
+function removeFloorControlActive(buttons) {
+    buttons.forEach(ele => ele.classList.remove('seat-title-active'));
+}
+
+function addFloorNone(floors) {
+    floors.forEach(ele => ele.classList.add('this-floor-not'));
+}
+
+function removeFloorNone(floors) {
+    floors.forEach(ele => ele.classList.remove('this-floor-not'));
+}
