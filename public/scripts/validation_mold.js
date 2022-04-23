165ceb097daa92f1527b444017d90386551df7d4
Nguyen Ngoc Huy <huynguyenffn@gmail.com>
2022-04-23 14:32:35 +0700

    start validation

---
+++ public/scripts/validation_mold.js
@@ -0,0 +1,10 @@
+const mainFom = document.querySelector('.form-validation');
+const submitButton = document.querySelector('.form-submit-validation');
+
+submitButton.onclick = () => {
+    onFormSubmit();
+}
+
+function onFormSubmit() {
+
+}
